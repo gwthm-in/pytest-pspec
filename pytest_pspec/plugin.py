d0f6b1ab9041ea22fc7f22c216c367941194b1f8
Gowtham Sai <gowtham95india@gmail.com>
2018-01-21 23:45:34 +0530

    Corrected as per code style

--- pytest_pspec/plugin.py
+++ pytest_pspec/plugin.py
@@ -29,6 +29,7 @@ def pytest_configure(config):
         config.pluginmanager.unregister(standard_reporter)
         config.pluginmanager.register(pspec_reporter, 'terminalreporter')
 
+
 def pytest_collection_modifyitems(config, items):
     for item in items:
         node = item.obj
@@ -36,6 +37,7 @@ def pytest_collection_modifyitems(config, items):
         suf = node.__doc__.strip() if node.__doc__ else node.__name__
         nodeid_parts[-1] = suf or nodeid_parts[-1]
         item._nodeid = '::'.join(nodeid_parts)
+
 
 class TestdoxTerminalReporter(TerminalReporter):
 
